e4d7db033abb24f28c7707b0c0638c2d9954de46
Sanket Shirolkar <snktshirolkar@gmail.com>
2022-08-20 16:59:33 +0530

    Added Introduction screen

--- src/components/introduction/Intro.jsx
+++ src/components/introduction/Intro.jsx
@@ -1,8 +1,24 @@
 import "./intro.scss";
 
+
 const Intro = () => {
   return (
-    <div className="intro" id="intro">Intro</div>
+    <div className="intro" id="intro">
+      <div className="left_intro">
+        <div className="imageContainer">
+          <img src="assets/avataaars.png" alt="Avataar"/>
+        </div>
+      </div>
+      <div className="right_intro">
+        <div className="wrapper_intro">
+          <h2>Hi There, I'm</h2>
+          <h1>Sanket Shirolkar</h1>
+          <h3>
+            I Love <span>Table Tennis</span> 
+          </h3>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,20 +4,20 @@ import {MdPerson, MdEmail} from "react-icons/md"
 const Navbar = () => {
   return (
     <div className="navbar">
-        <div className="wrapper">
-            <div className="left">
-                <a href="#intro" className="logo">Hey.</a>
-                <div className="itemContainer">
+        <div className="wrapper_nav">
+            <div className="left_nav">
+                <a href="#intro" className="logo">TBD</a>
+                <div className="itemContainer_nav">
                     <MdPerson className="icon" />
                     <span>+919538419526</span>
                     
                 </div>
-                <div className="itemContainer">
+                <div className="itemContainer_nav">
                     <MdEmail className="icon" />
                     <span>snktshirolkar@gmail.com</span> 
                 </div>
             </div>
-            <div className="right">
+            <div className="right_nav">
 
             </div>
         </div>
